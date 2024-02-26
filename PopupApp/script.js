33f08650dc27144d3197838cbe3d887524009504
RakibTheDevMosaic <devdreamweaver@gmail.com>
2024-02-26 12:17:28 +0600

    popup

---
+++ PopupApp/script.js
@@ -0,0 +1,9 @@
+let popupDiv = document.getElementById('popupDiv');
+
+function openpopup() {
+    popupDiv.classList.add('open-popup');
+}
+
+function closepopup() {
+    popupDiv.classList.remove('open-popup');
+}

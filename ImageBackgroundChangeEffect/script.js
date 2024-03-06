45bdf81e344592db332c4e1232a06cbd14819686
RakibTheDevMosaic <devdreamweaver@gmail.com>
2024-03-06 15:23:59 +0600

    imagebackgroundchange

---
+++ ImageBackgroundChangeEffect/script.js
@@ -0,0 +1,20 @@
+var ImgBox = document.querySelector(".imgbox");
+var ImgWrap = document.querySelector(".imgWrap");
+
+var originalImg = document.getElementById('originalImg');
+var line = document.getElementById('line');
+originalImg.style.width = ImgBox.offsetWidth + "px";
+
+var leftSpace = ImgBox.offsetLeft;
+
+
+ImgBox.onmousemove = function(e){
+    var boxWidth = (e.pageX - leftSpace) + "px";
+    ImgWrap.style.width = boxWidth;
+    line.style.left = boxWidth;
+}
+
+
+
+
+
